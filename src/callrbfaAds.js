2a24b61b9e13353ada2e7fab29e1700245db3621
Kevin Voortmans <kevin.voortmans@ipmgroup.be>
2021-06-18 09:11:12 +0200

    test commit voor oli

--- src/callrbfaAds.js
+++ src/callrbfaAds.js
@@ -3,22 +3,12 @@ var InitAdserver = function(__tcfapi){
   __tcfapi('addEventListener', 2, function(tcData, success){
     if(success && (tcData.eventStatus === 'tcloaded' || tcData.eventStatus === 'useractioncomplete') ) {
         window.rbfaAds = new RBFAads($, googletag, tcData.tcString).init();
-        // remove the ourself to not get called more than once
         __tcfapi('removeEventListener', 2, (success) => {}, tcData.listenerId);
 
     }       
   });
 }
- /*var waitForConsent = function () {
-  if(typeof __tcfapi !== "undefined"){
-    InitAdserver(__tcfapi);
-  }
-  else{
-    setTimeout(waitForConsent, 5);
-  };
-};
-waitForConsent(); */
-
+// Test test
 if(typeof __tcfapi !== "undefined"){
   InitAdserver(__tcfapi);
 }else{
